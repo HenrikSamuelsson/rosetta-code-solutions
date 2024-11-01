98294d6cc259644ace2909a255e0a712b955ba42
Henrik Samuelsson <henrik.samuelsson@gmail.com>
2024-11-01 19:07:15 +0100

    Disable warning on usage of scanf.

---
+++ a-plus-b/a_plus_b.h
@@ -0,0 +1,3 @@
+#ifndef a_plus_b_h
+#define a_plus_b_h
+#endif /* a_plus_b_h */
--- a-plus-b/main.c
+++ a-plus-b/main.c
@@ -1,18 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS
+
 #include <stdio.h>
 #include <stdlib.h>
 
 int a_plus_b(int a, int b);
 
-int main(void)
-{
-    int a;
-    int b;
-    scanf("%d%d", &a, &b);
-    printf("%d", a_plus_b(a, b));
-    return EXIT_SUCCESS;
+int main(void) {
+  int a;
+  int b;
+  scanf("%d%d", &a, &b);
+  printf("%d", a_plus_b(a, b));
+  return EXIT_SUCCESS;
 }
 
-int a_plus_b(int a, int b)
-{
-    return a + b;
-}
+int a_plus_b(int a, int b) { return a + b; }
