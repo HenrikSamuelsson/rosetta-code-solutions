3dd272124fcd5876505a47d7d849d1f18e663842
Henrik Samuelsson <henrik.samuelsson@gmail.com>
2024-11-06 19:18:48 +0100

    Add tests for some doubles.

--- boolean-values/main.c
+++ boolean-values/main.c
@@ -3,12 +3,20 @@
 #define SIZE 3
 
 int main(void) {
-  const int numbers[SIZE] = {42, 0, -42};
+  const int integers[SIZE] = {-42, 0, 42};
   for (size_t i = 0; i < SIZE; i++) {
-    if (numbers[i]) {
-      printf("%d equals true\n", numbers[i]);
+    if (integers[i]) {
+      printf("%d equals true\n", integers[i]);
     } else {
-      printf("%d equals false\n", numbers[i]);
+      printf("%d equals false\n", integers[i]);
+    }
+  }
+  const double decimals[SIZE] = {-1.0, 0.0, 1.0};
+   for (size_t i = 0; i < SIZE; i++) {
+    if (decimals[i]) {
+      printf("%f equals true\n", decimals[i]);
+    } else {
+      printf("%f equals false\n", decimals[i]);
     }
   }
 }
