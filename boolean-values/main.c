c74083eeef1f65ffc7098352c91f1bf9cab53de1
Henrik Samuelsson <henrik.samuelsson@gmail.com>
2024-11-09 10:54:38 +0100

    Update the code for boolean values exercise.

--- boolean-values/main.c
+++ boolean-values/main.c
@@ -1,22 +1,36 @@
+/**
+ * \file main.c
+ *
+ * \brief Shows how to represent the boolean states true and false in the C
+ * programming language.
+ *
+ * Not how everything that is zero maps to false and non-zero values maps to
+ * true.
+ */
+#include <assert.h>
+#include <stdbool.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-#define SIZE 3
-
+/**
+ * \brief Entry point and only function of this program.
+ *
+ * \return int
+ */
 int main(void) {
-  const int integers[SIZE] = {-42, 0, 42};
-  for (size_t i = 0; i < SIZE; i++) {
-    if (integers[i]) {
-      printf("%d equals true\n", integers[i]);
-    } else {
-      printf("%d equals false\n", integers[i]);
-    }
-  }
-  const double decimals[SIZE] = {-1.0, 0.0, 1.0};
-   for (size_t i = 0; i < SIZE; i++) {
-    if (decimals[i]) {
-      printf("%f equals true\n", decimals[i]);
-    } else {
-      printf("%f equals false\n", decimals[i]);
-    }
-  }
+  // Check integers.
+  assert(false == (bool)0);
+  assert(true == (bool)4);
+  assert(true == (bool)-4);
+  // Check real numbers.
+  assert(false == (bool)0.0);
+  assert(true == (bool)4.0);
+  assert(true == (bool)-4.0);
+  // Check characters.
+  assert(false == (bool)'\0');
+  assert(true == (bool)'0');
+  assert(true == (bool)'4');
+  assert(true == (bool)-'4');
+  printf("All assertions passed.");
+  return EXIT_SUCCESS;
 }
