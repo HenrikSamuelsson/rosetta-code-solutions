81b9e4ac3057e628c8eb078dc331fc3617f97c27
Henrik Samuelsson <henrik.samuelsson2@saabgroup.com>
2024-11-11 13:25:39 +0100

    Update documentation again.

--- address-of-a-variable/main.c
+++ address-of-a-variable/main.c
@@ -3,11 +3,15 @@
  * 
  * \brief Demonstrates how to get the address of a variable.
  *
- * As seen in the code we gan get the address of an variable. But the other way
- * around, setting the address of an variable is in general not possible. It
- * should however be noted that there are compiler extensions that do enables 
- * us to set an address of an variable but this is beyond the scope of this 
- * exercise.
+ * As seen in the code we can get the address of an variable. But the other way
+ * around, setting the address of an variable, is in general not possible in C.
+ * Hence there is here no code that attempts to assign a variable to a given
+ * address.
+ *
+ * It should however be noted that there are compiler extensions that do enable 
+ * us to set an address of an variable. This can be required when working with 
+ * low level code running on embedded systems. But this is beyond the scope of 
+ * this exercise.
  */
 #include <inttypes.h>
 #include <stdlib.h>
