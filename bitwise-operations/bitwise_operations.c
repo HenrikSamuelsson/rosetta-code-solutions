a517ed143cb0b58319ba2c63425f87439f5a1be7
Henrik Samuelsson <henrik.samuelsson@gmail.com>
2024-11-13 19:03:39 +0100

    Add function to calculate bitwise_or.

--- bitwise-operations/bitwise_operations.c
+++ bitwise-operations/bitwise_operations.c
@@ -7,9 +7,9 @@
 /**
  * \brief Bitwise AND.
  *
- * Compares each bit of the first operand to the corresponding bit of the
- * second operand. If both bits are 1, the corresponding result bit is set to
- * 1. Otherwise, the corresponding result bit is set to 0.
+ * Compares each bit of the first operand to the corresponding bit of the second
+ * operand. If both bits are 1, the corresponding result bit is set to 1. 
+ * Otherwise, the corresponding result bit is set to 0.
  *
  * \param a First operand.
  * \param b Second operand.
@@ -18,5 +18,22 @@
 int bitwise_and(int a, int b) {
   int result;
   result = a & b;
+  return result;
+}
+
+/**
+ * \brief Bitwise OR.
+ *
+ * Compares each bit of the first operand to the corresponding bit of the second
+ * operand. If both bits are 0, the corresponding result bit is set to 0.
+ * Otherwise, the corresponding result bit is set to 0.
+ *
+ * \param a First operand.
+ * \param b Second operand.
+ * \return int Result of bitwise OR.
+ */
+int bitwise_or(int a, int b) {
+  int result;
+  result = a | b;
   return result;
 }
