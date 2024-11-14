0922e63726bc4b5147bbce7d7b2ca8c0fdb7854b
Henrik Samuelsson <henrik.samuelsson2@saabgroup.com>
2024-11-14 06:31:58 +0100

    Add macro to be able to print binary.

--- bitwise-operations/main.c
+++ bitwise-operations/main.c
@@ -5,10 +5,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define BYTE_TO_BINARY_PATTERN "%c%c%c%c%c%c%c%c"
+#define BYTE_TO_BINARY(byte)  \
+  ((byte) & 0x80 ? '1' : '0'), \
+  ((byte) & 0x40 ? '1' : '0'), \
+  ((byte) & 0x20 ? '1' : '0'), \
+  ((byte) & 0x10 ? '1' : '0'), \
+  ((byte) & 0x08 ? '1' : '0'), \
+  ((byte) & 0x04 ? '1' : '0'), \
+  ((byte) & 0x02 ? '1' : '0'), \
+  ((byte) & 0x01 ? '1' : '0') 
+
 int main(void) {
   int a;
   int b;
   scanf("%d%d", &a, &b);
-  printf("%d", bitwise_and(a, b));
+  int a_and_b = bitwise_and(a, b);
+  printf("%d ", a_and_b);
+  printf(BYTE_TO_BINARY_PATTERN, BYTE_TO_BINARY(a_and_b));
   return EXIT_SUCCESS;
 }
