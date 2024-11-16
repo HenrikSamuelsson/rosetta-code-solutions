7276a7664734dc83fa12bd235e79e7934bc84623
Henrik Samuelsson <henrik.samuelsson@gmail.com>
2024-11-16 09:57:20 +0100

    Add function to display results.
    This new function reduces code duplication.

--- bitwise-operations/main.c
+++ bitwise-operations/main.c
@@ -1,38 +1,39 @@
 #define _CRT_SECURE_NO_WARNINGS
 
-#include "bitwise_operations.h"
-
 #include <stdio.h>
 #include <stdlib.h>
 
-#define BYTE_TO_BINARY_PATTERN "%c%c%c%c %c%c%c%c"
-#define BYTE_TO_BINARY(byte)  \
-  ((byte) & 0x80 ? '1' : '0'), \
-  ((byte) & 0x40 ? '1' : '0'), \
-  ((byte) & 0x20 ? '1' : '0'), \
-  ((byte) & 0x10 ? '1' : '0'), \
-  ((byte) & 0x08 ? '1' : '0'), \
-  ((byte) & 0x04 ? '1' : '0'), \
-  ((byte) & 0x02 ? '1' : '0'), \
-  ((byte) & 0x01 ? '1' : '0') 
+#include "bitwise_operations.h"
 
-int main(void) {
+#define BYTE_TO_BINARY_PATTERN "%c%c%c%c %c%c%c%c"
+#define BYTE_TO_BINARY(byte)                                \
+  ((byte)&0x80 ? '1' : '0'), ((byte)&0x40 ? '1' : '0'),     \
+      ((byte)&0x20 ? '1' : '0'), ((byte)&0x10 ? '1' : '0'), \
+      ((byte)&0x08 ? '1' : '0'), ((byte)&0x04 ? '1' : '0'), \
+      ((byte)&0x02 ? '1' : '0'), ((byte)&0x01 ? '1' : '0')
+
+struct operands {
   int a;
   int b;
-  scanf("%d%d", &a, &b);
-  printf("%d\t", a);
-  printf(BYTE_TO_BINARY_PATTERN, BYTE_TO_BINARY(a));
-  printf("\ta\n");
-  printf("%d\t", b);
-  printf(BYTE_TO_BINARY_PATTERN, BYTE_TO_BINARY(b));
-  printf("\tb\n");
-  int a_and_b = bitwise_and(a, b);
-  printf("%d\t", a_and_b);
-  printf(BYTE_TO_BINARY_PATTERN, BYTE_TO_BINARY(a_and_b));
-  printf("\ta AND b\n");
-  int a_or_b = bitwise_or(a, b);
-  printf("%d\t", a_or_b);
-  printf(BYTE_TO_BINARY_PATTERN, BYTE_TO_BINARY(a_or_b));
-  printf("\ta OR b\n");
+};
+
+static void display_result(char * operation_name, int result);
+
+int main(void) {
+  struct operands ops; 
+  scanf("%d%d", &ops.a, &ops.b);
+  display_result("a", ops.a);
+  display_result("b", ops.b);
+  int a_and_b = bitwise_and(ops.a, ops.b);
+  display_result("a AND b", a_and_b);
+  int a_or_b = bitwise_or(ops.a, ops.b);
+  display_result("a OR B", a_or_b);
   return EXIT_SUCCESS;
+}
+
+static void display_result(char * operation_name, int result) {
+  printf("%s\t", operation_name);
+  printf("%d\t", result);
+  printf(BYTE_TO_BINARY_PATTERN, BYTE_TO_BINARY(result));
+  printf("\n");
 }
